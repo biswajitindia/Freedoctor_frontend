6ca8f961f46bf4a3c01fc5e45a781177b7d12d5f
biswajitindia <biswajitsahu1472@gmail.com>
2025-07-27 10:07:14 +0530

    server add

--- vite.config.js
+++ vite.config.js
@@ -21,7 +21,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Your backend server
+        target: 'https://freedoctor-backend.vercel.app/', // Your backend server
         changeOrigin: true,
         secure: false,
       },
